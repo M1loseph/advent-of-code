16d2143d92ff7a2f244ef19f16777b61601dda1a
M1loseph <milosz.dyka@gmail.com>
2023-12-11 17:25:44 +0100

    2023 day 1 / puzzle 2

---
+++ 2023/01/puzzle_2/src/main.rs
@@ -0,0 +1,88 @@
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+fn find_first_digit(string: &String) -> u32 {
+    let digits_names = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    let (mut position, mut current_value) = string
+        .chars()
+        .enumerate()
+        .filter(|(_, c)| c.is_digit(10))
+        .map(|(i, c)| (i, c.to_digit(10).unwrap()))
+        .next()
+        .unwrap();
+
+    for (name, value) in digits_names {
+        if let Some(name_position) = string.find(name) {
+            if name_position < position {
+                position = name_position;
+                current_value = value;
+            }
+        }
+    }
+    current_value
+}
+
+fn find_last_digit(string: &String) -> u32 {
+    let digits_names = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
+    let (mut position, mut current_value) = string
+        .chars()
+        .enumerate()
+        .filter(|(_, c)| c.is_digit(10))
+        .map(|(i, c)| (i, c.to_digit(10).unwrap()))
+        .last()
+        .unwrap();
+
+    for (name, value) in digits_names {
+        if let Some(name_position) = string.rfind(name) {
+            if name_position > position {
+                position = name_position;
+                current_value = value;
+            }
+        }
+    }
+    current_value
+}
+
+fn main() {
+    let file = match File::open("input/input.txt") {
+        Ok(result) => result,
+        Err(_) => panic!("Should have opened a file!"),
+    };
+
+    let sum: u32 = BufReader::new(file)
+        .lines()
+        .filter_map(|line| line.ok())
+        .map(|line| {
+            let first_digit = find_first_digit(&line);
+            let last_digit = find_last_digit(&line);
+            println!("{line} -> {first_digit}{last_digit}");
+            first_digit * 10 + last_digit
+        })
+        .sum();
+    println!("===================================");
+    println!("Sum -> {sum}");
+}
