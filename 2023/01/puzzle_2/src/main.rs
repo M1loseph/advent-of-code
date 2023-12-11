95b03b157a0e1904b0a8d599f57cfc555bc51441
M1loseph <milosz.dyka@gmail.com>
2023-12-11 17:33:56 +0100

    2023 day 1 / puzzle 2 refactor

--- 2023/01/puzzle_2/src/main.rs
+++ 2023/01/puzzle_2/src/main.rs
@@ -3,19 +3,7 @@ use std::fs::File;
 use std::io::BufRead;
 use std::io::BufReader;
 
-fn find_first_digit(string: &String) -> u32 {
-    let digits_names = HashMap::from([
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ]);
-
+fn find_first_digit(string: &String, digit_names: &HashMap<&str, u32>) -> u32 {
     let (mut position, mut current_value) = string
         .chars()
         .enumerate()
@@ -24,30 +12,18 @@ fn find_first_digit(string: &String) -> u32 {
         .next()
         .unwrap();
 
-    for (name, value) in digits_names {
+    for (name, value) in digit_names {
         if let Some(name_position) = string.find(name) {
             if name_position < position {
                 position = name_position;
-                current_value = value;
+                current_value = *value;
             }
         }
     }
     current_value
 }
 
-fn find_last_digit(string: &String) -> u32 {
-    let digits_names = HashMap::from([
-        ("one", 1),
-        ("two", 2),
-        ("three", 3),
-        ("four", 4),
-        ("five", 5),
-        ("six", 6),
-        ("seven", 7),
-        ("eight", 8),
-        ("nine", 9),
-    ]);
-
+fn find_last_digit(string: &String, digit_names: &HashMap<&str, u32>) -> u32 {
     let (mut position, mut current_value) = string
         .chars()
         .enumerate()
@@ -56,11 +32,11 @@ fn find_last_digit(string: &String) -> u32 {
         .last()
         .unwrap();
 
-    for (name, value) in digits_names {
+    for (name, value) in digit_names {
         if let Some(name_position) = string.rfind(name) {
             if name_position > position {
                 position = name_position;
-                current_value = value;
+                current_value = *value;
             }
         }
     }
@@ -73,12 +49,24 @@ fn main() {
         Err(_) => panic!("Should have opened a file!"),
     };
 
+    let digit_names = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+    ]);
+
     let sum: u32 = BufReader::new(file)
         .lines()
         .filter_map(|line| line.ok())
         .map(|line| {
-            let first_digit = find_first_digit(&line);
-            let last_digit = find_last_digit(&line);
+            let first_digit = find_first_digit(&line, &digit_names);
+            let last_digit = find_last_digit(&line, &digit_names);
             println!("{line} -> {first_digit}{last_digit}");
             first_digit * 10 + last_digit
         })
